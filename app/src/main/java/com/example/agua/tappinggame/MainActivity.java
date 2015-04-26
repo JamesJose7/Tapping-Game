e98ae970229dc89001ff00d8260ed3cf1002592d
Jose <pepe.96.jose@gmail.com>
2015-04-26 14:04:01 -0500

    Add onClick for every button

--- app/src/main/java/com/example/agua/tappinggame/MainActivity.java
+++ app/src/main/java/com/example/agua/tappinggame/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.agua.tappinggame;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 
@@ -115,57 +117,261 @@ public class MainActivity extends Activity {
 
         Button[] buttons = {
 
-        mCenter;
-        mLeft1;
-        mLeft2;
-        mLeft3;
-        mRight1;
-        mRight2;
-        mRight3;
-        mTop1;
-        mTop2;
-        mTop3;
-        mBottom1;
-        mBottom2;
-        mBottom3;
-        mBottomLeft01;
-        private Button mBottomLeft02;
-        private Button mBottomLeft03;
-        private Button mBottomLeft11;
-        private Button mBottomLeft12;
-        private Button mBottomLeft13;
-        private Button mBottomLeft21;
-        private Button mBottomLeft22;
-        private Button mBottomLeft23;
-        private Button mBottomRight01;
-        private Button mBottomRight02;
-        private Button mBottomRight03;
-        private Button mBottomRight11;
-        private Button mBottomRight12;
-        private Button mBottomRight13;
-        private Button mBottomRight21;
-        private Button mBottomRight22;
-        private Button mBottomRight23;
-        private Button mTopLeft01;
-        private Button mTopLeft02;
-        private Button mTopLeft03;
-        private Button mTopLeft11;
-        private Button mTopLeft12;
-        private Button mTopLeft13;
-        private Button mTopLeft21;
-        private Button mTopLeft22;
-        private Button mTopLeft23;
-        private Button mTopRight01;
-        private Button mTopRight02;
-        private Button mTopRight03;
-        private Button mTopRight11;
-        private Button mTopRight12;
-        private Button mTopRight13;
-        private Button mTopRight21;
-        private Button mTopRight22;
-        private Button mTopRight23;
+        mCenter,
+        mLeft1,
+        mLeft2,
+        mLeft3,
+        mRight1,
+        mRight2,
+        mRight3,
+        mTop1,
+        mTop2,
+        mTop3,
+        mBottom1,
+        mBottom2,
+        mBottom3,
+        mBottomLeft01,
+        mBottomLeft02,
+        mBottomLeft03,
+        mBottomLeft11,
+        mBottomLeft12,
+        mBottomLeft13,
+        mBottomLeft21,
+        mBottomLeft22,
+        mBottomLeft23,
+        mBottomRight01,
+        mBottomRight02,
+        mBottomRight03,
+        mBottomRight11,
+        mBottomRight12,
+        mBottomRight13,
+        mBottomRight21,
+        mBottomRight22,
+        mBottomRight23,
+        mTopLeft01,
+        mTopLeft02,
+        mTopLeft03,
+        mTopLeft11,
+        mTopLeft12,
+        mTopLeft13,
+        mTopLeft21,
+        mTopLeft22,
+        mTopLeft23,
+        mTopRight01,
+        mTopRight02,
+        mTopRight03,
+        mTopRight11,
+        mTopRight12,
+        mTopRight13,
+        mTopRight21,
+        mTopRight22,
+        mTopRight23
 
         };
+    }
+
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.cellCenter:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellLeft1:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellLeft2:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellLeft3:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellRight1:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellRight2:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellRight3:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTop1:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTop2:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTop3:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottom1:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottom2:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottom3:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopRight01:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopRight02:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopRight03:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopRight11:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopRight12:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopRight13:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopRight21:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopRight22:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopRight23:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopLeft01:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopLeft02:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopLeft03:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopLeft11:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopLeft12:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopLeft13:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopLeft21:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopLeft22:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellTopLeft23:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomLeft01:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomLeft02:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomLeft03:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomLeft11:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomLeft12:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomLeft13:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomLeft21:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomLeft22:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomLeft23:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomRight01:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomRight02:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomRight03:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomRight11:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomRight12:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomRight13:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomRight21:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomRight22:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+            case R.id.cellBottomRight23:
+                view.setBackgroundColor(Color.parseColor("#ffffff"));
+                break;
+
+
+        }
+
     }
 
 
