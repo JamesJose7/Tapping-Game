e36b2d6d489c109de4b4aa01a42c3215dfcf7270
Jose <pepe.96.jose@gmail.com>
2015-04-28 16:34:01 -0500

    Remove default layout folder

--- app/src/main/java/com/example/agua/tappinggame/MainActivity.java
+++ app/src/main/java/com/example/agua/tappinggame/MainActivity.java
@@ -183,7 +183,7 @@ public class MainActivity extends Activity {
                 mTopRight23
         };
 
-        String[] colorPalette = {"#a04d7f17", "#a06bb120"};
+        String[] colorPalette = {"#D32F2F", "#FFEB3B"};
 
 
         for (int i = 0; i < buttons.length; i++) {
